100d18305a07417844546d0c982d59be15e09d98
Christian Grimberg <christian.grimberg@outlook.com>
2022-09-29 00:35:02 +0000

    :+1: Se prueba una Azure Functions HttpTimmer.

--- labs/class03/azure-function/Echo.cs
+++ labs/class03/azure-function/Echo.cs
@@ -8,28 +8,17 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace Company.Function
+namespace azure_function
 {
     public static class Echo
     {
         [FunctionName("Echo")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static IActionResult Run(
+            [HttpTrigger("POST")] HttpRequest request,
+            ILogger logger)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(value: name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            logger.LogInformation("Request recibido");
+            return new OkObjectResult(request.Body);
         }
     }
 }
