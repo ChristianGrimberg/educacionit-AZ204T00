3661fd6cebe98b7b12d26bcf7cf5f15762153f66
Christian Grimberg <christian.grimberg@outlook.com>
2022-10-26 23:59:36 +0000

    :+1: Se carga el sitio de autenticacion con Azure de la Clase 7.

---
+++ labs/class07/AwesomeAppChristianGrimberg/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AwesomeAppChristianGrimberg.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
