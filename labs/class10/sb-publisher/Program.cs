6cebbfa9bde120487676af231e1548dd0e0aa76b
Christian Grimberg <christian.grimberg@outlook.com>
2022-11-23 23:25:42 +0000

    :+1: Se prueba el servicio de Azure Service Bus enviando mensajes desde la consola de .NET

---
+++ labs/class10/sb-publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Azure.Messaging.ServiceBus;
+
+const string connectionString = "<ServiceBus Connection String>";
+const string queueName = "messagequeue";
+const int numOfMessages = 30;
+
+ServiceBusClient client = new ServiceBusClient(connectionString);
+ServiceBusSender sender = client.CreateSender(queueName);
+ServiceBusMessageBatch manyMessages = await sender.CreateMessageBatchAsync();
+
+for (int i = 0; i < numOfMessages; i++)
+{
+    manyMessages.TryAddMessage(new ServiceBusMessage($"Mensaje desde .NET nro {i}"));
+}
+
+try
+{
+    await sender.SendMessagesAsync(manyMessages);
+    Console.WriteLine("Mensaje enviado");
+
+}
+finally
+{
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
