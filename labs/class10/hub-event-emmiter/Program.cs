6ce3f51d03baa838a90c8265e3c78401360a4f74
Christian Grimberg <christian.grimberg@outlook.com>
2022-11-24 00:38:15 +0000

    :+1: Se emite eventos hacia Azure EventHub.

---
+++ labs/class10/hub-event-emmiter/Program.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using System;
+
+const string connectionString = "<EventHub Connection String>";
+const string hubName = "hub4chris";
+
+EventHubProducerClient client = new EventHubProducerClient(connectionString, hubName);
+EventDataBatch manyEvents = await client.CreateBatchAsync();
+
+for (int i = 0; i < 1000; i++)
+{
+    manyEvents.TryAdd(new EventData($"Evento {i}"));
+}
+
+try
+{
+    await client.SendAsync(manyEvents);
+    Console.WriteLine("Se enviaron muchos eventos");
+}
+finally
+{
+    await client.DisposeAsync();
+}
